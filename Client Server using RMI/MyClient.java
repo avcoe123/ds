e27e922567b3eee9e484287eeb8c63afdd97f73d
avcoe123 <153625624+avcoe123@users.noreply.github.com>
2026-05-14 23:34:59 +0530

    Create MyClient.java

---
+++ Client Server using RMI/MyClient.java
@@ -0,0 +1,19 @@
+#MyClient.java
+
+public class MyClient {
+
+    public static void main(String[] args) {
+
+        try {
+
+            MyInterface obj = (MyInterface)
+                    java.rmi.Naming.lookup("//localhost/MyRemoteClass");
+
+            System.out.println(obj.sayHello());
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+}
